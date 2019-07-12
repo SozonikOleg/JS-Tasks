81978065e30b0ee5791c20bccec1fdfdcf13064a
SozonikOleg <sozonik999@gmail.com>
2019-07-12 18:09:34 +0300

    feat:add file with string tasks

--- src/tasks/01-string-tasks.js
+++ src/tasks/01-string-tasks.js
@@ -1,1 +1,21 @@
-console.log('oleg');
+console.log('**********STRINGS***********');
+
+/**  1) 'aaa@bbb@ccc' => get string 'aaa!bbb!ccc'; */
+const str = 'aaa@bbb@ccc';
+console.log('task 1:', str.replace(/@/g, '!'));
+
+
+/** 2) 'aaa bbb ccc' => get string 'bbb'; */
+const data = 'aaa bbb ccc';
+console.log('task 2:', data.substring(4, 7));
+
+
+/** 3) 'ololo fdfsdf osdsdfs sfs' => get string 'fdfsdf osdsdfs sfs'; */
+const string = 'ololo fdfsdf osdsdfs sfs';
+const newString = string.substring(6);
+console.log('task 3:', newString);
+
+/** 4) 'stringify' => get string 'ring'; */
+const str4 = 'stringify';
+const newStr4 = str4.substr(3, 4);
+console.log(newStr4);
